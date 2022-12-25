5ecd9d644ab16bae77525631283c633a49d41343
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-25 20:53:38 -0300

    exibindo resultado na tela

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute')
+
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -7,5 +9,13 @@ recognition.start()
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    console.log(e.results[0][0].transcript)
+    chute = e.results[0][0].transcript
+    exibeChuteNTela(chute)
+}
+
+function exibeChuteNTela(chute) {
+    elementoChute.innerHTML = `
+     <div>Você disse</div>
+     <span class="box">${chute}</span>   
+    `
 }
