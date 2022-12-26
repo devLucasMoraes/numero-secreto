d39cc39caa6e8c9b64c57375dc6c4fb26d07bf36
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-25 21:28:13 -0300

    validacao se o numero e maior ou menor

--- app/validacao.js
+++ app/validacao.js
@@ -2,15 +2,25 @@ function verificaSeChutePossuiValorValido(chute){
     const numero = +chute
     if(chuteInvalido(numero)){
         elementoChute.innerHTML += '<div>valor inválido</div>'
+        return
     }
     if(numeroMaiorOuMenorQuePermitido(numero)){
         elementoChute.innerHTML += `<div>valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
     }
     if(numero === numeroSecreto){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
         `
+    } else if(numero < numeroSecreto){
+        elementoChute.innerHTML += `
+            <div>O número secreto e maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto e menor <i class="fa-solid fa-down-long"></i></div>
+    `
     }
 }
 
